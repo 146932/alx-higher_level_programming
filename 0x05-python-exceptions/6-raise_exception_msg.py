9d8ff203258ad11fdca0d7cc51c48f1aafce1d99
Gbadebotemitope77@yahoo.com <Gbadebotemitope77@yahoo.com>
2022-12-19 20:06:44 -0800

    6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Temitope O. Gbadebo <gtemiope28@gmail.com>
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
+
